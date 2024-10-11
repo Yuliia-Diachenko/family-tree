651532a2ac93f871cf45c8dec75401b310328aea
Yuliia Diachenko <zirkanew82@gmail.com>
2024-10-11 23:34:15 +0300

    add validation

--- src/validation/family.js
+++ src/validation/family.js
@@ -11,22 +11,22 @@ export const createPersonSchema = Joi.object({
         'number.base': 'Age should be a number',
         'any.required': 'Age is required',
       }),
-    parents: Joi.string.min(3).max(30).messages({
+    parents: Joi.string().min(3).max(30).messages({
         'string.base': 'Parents should be a string',
         'string.min': 'Parents should have at least {#limit} characters',
         'string.max': 'Parents should have at most {#limit} characters',
       }),
-    ancestors: Joi.string.min(3).max(30).messages({
+    ancestors: Joi.string().min(3).max(30).messages({
         'string.base': 'Ancestors should be a string',
         'string.min': 'Ancestors should have at least {#limit} characters',
         'string.max': 'Ancestors should have at most {#limit} characters',
       }),
-    children: Joi.string.min(3).max(30).messages({
+    children: Joi.string().min(3).max(30).messages({
         'string.base': 'Children should be a string',
         'string.min': 'Children should have at least {#limit} characters',
         'string.max': 'Children should have at most {#limit} characters',
       }),
-    grandchildren: Joi.string.min(3).max(30).messages({
+    grandchildren: Joi.string().min(3).max(30).messages({
         'string.base': 'Grandchildren should be a string',
         'string.min': 'Grandchildren should have at least {#limit} characters',
         'string.max': 'Grandchildren should have at most {#limit} characters',
